10ab7a0834a5856ceacb3b6735f0beb0ef4bc712
José Alberto Jiménez Sánchez <elautenticopanzeco@hotmail.com>
2015-08-02 19:46:27 +0200

    Editar preguntas
    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -51,10 +51,32 @@ exports.new = function(req,res){
 	res.render('quizes/new', {quiz: quiz});
 };
 
+exports.edit = function(req,res){
+	var quiz = req.quiz;
+	res.render('quizes/edit', {quiz: quiz, errors:[]});
+};
+
 exports.create = function(req,res){
 	var quiz = models.Quiz.build(req.body.quiz);
 
 	quiz.save({fields:["pregunta","respuesta"]}).then(function(){
 		res.redirect("/quizes");
 	})
+};
+
+exports.update = function(req,res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(
+		function(err){
+			if (err){
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			}else{
+				req.quiz.save({fields:["pregunta","respuesta"]}).then(
+					function(){ res.redirect('/quizes');}
+				);
+			}
+		}
+	);
 };
