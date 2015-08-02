de6486401c04b7e1e67a797239f3d48a0bbc91fc
José Alberto Jiménez Sánchez <elautenticopanzeco@hotmail.com>
2015-08-02 19:35:39 +0200

    Crear preguntas
    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -42,3 +42,19 @@ exports.show = function(req,res){
 	}
 	res.render('quizes/answer', {quiz: req.quiz,respuesta: resultado});
  };
+
+exports.new = function(req,res){
+	var quiz = models.Quiz.build(
+		{pregunta:"Pregunta", respuesta:"Respuesta"}
+	);
+
+	res.render('quizes/new', {quiz: quiz});
+};
+
+exports.create = function(req,res){
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	quiz.save({fields:["pregunta","respuesta"]}).then(function(){
+		res.redirect("/quizes");
+	})
+};
